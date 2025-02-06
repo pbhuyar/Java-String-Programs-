4fbbb1d9920557dcd266a141b7062897bf12a317
pbhuyar <131512700+pbhuyar@users.noreply.github.com>
2025-02-06 14:57:24 +0530

    Add files via upload
    Ways to Create String Object in java

---
+++ StringObjectCreation.java
@@ -0,0 +1,21 @@
+class StringObjectCreation
+{
+	public static void main (String args [] )
+	{
+	String s1 = new String ("India");
+	System.out.println(s1);
+	String s2 = "India";
+	System.out.println(s1);
+	char c [] = {'I','N','D','I','A'};
+	String s3 = new String (c);
+	int size = "India".length();
+	System.out.println(size);
+	char c1 = new String ("India").charAt(2);
+	System.out.println(c);
+	System.out.println("India");
+	}
+}
+	
+
+
+	
